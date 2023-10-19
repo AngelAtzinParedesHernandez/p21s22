f177726f6e866eba5a4fc2a6f20002a1a75604e6
AngelAtzinParedesHernandez <144190833+AngelAtzinParedesHernandez@users.noreply.github.com>
2023-10-18 19:20:48 -0600

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,9 @@
+ public class P2p71S22{
+ 	public static void main(String[] args) {
+ 		System.out.println("El numero de parametros ingresados desde linea de cuando "+ args.length);
+ 		for(int a=0;a<args.length;a++){
+ 			System.out.println("Valor #"+a+ ": "+args{a});
+ 			
+ 		}
+ 	}
+ }
